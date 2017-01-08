1d2fd997886f516619f7eaf04c3dfae5583b9c50
Ant Zucaro <azucaro@gmail.com>
2017-01-08 09:36:51 -0500

    Add weapon damage stacked bar chart.

--- xonotic_by_the_numbers.py
+++ xonotic_by_the_numbers.py
@@ -3,12 +3,42 @@
 import argparse
 import datetime
 import os
+import pdb
 import sys
 
 import numpy as np
 import pandas as pd
 import psycopg2 as pg
 import matplotlib.pyplot as plt
+
+# the same weapon colors from XonStat
+weapon_colors = {
+    "arc": "#b8e9ff",
+    "laser": "#ff5933",
+    "blaster": "#ff5933",
+    "shotgun": "#1f77b4",
+    "uzi": "#b9e659",
+    "machinegun": "#b9e659",
+    "grenadelauncher": "#ff2600",
+    "mortar": "#ff2600",
+    "minelayer": "#bfbf00",
+    "electro": "#597fff",
+    "crylink": "#d940ff",
+    "nex": "#00e6ff",
+    "vortex": "#00e6ff",
+    "hagar": "#d98059",
+    "rocketlauncher": "#ffbf33",
+    "devastator": "#ffbf33",
+    "porto": "#7fff7f",
+    "minstanex": "#d62728",
+    "vaporizer": "#d62728",
+    "hook": "#a5ffd8",
+    "hlac": "#ffa533",
+    "seeker": "#ff5959",
+    "rifle": "#9467bd",
+    "tuba": "#d87f3f",
+    "fireball": "#33ff33"
+}
 
 
 def games_per_month(conn, year):
@@ -227,6 +257,103 @@ def hours_played(conn, year):
     plt.savefig("{}_hours_heatmap.png".format(year))
 
 
+def weapon_damage_per_month(conn, year):
+    """Weapon damage per month."""
+
+    # read the data into a dataframe from the database
+    sql = """
+        select pws.weapon_cd,
+           date_trunc('month', pws.create_dt),
+           to_char(create_dt, 'Mon') "month",
+           sum(pws.actual) "actual"
+        from player_weapon_stats pws join cd_weapon cd on pws.weapon_cd = cd.weapon_cd
+        where pws.create_dt between '2016-01-01' and '2017-01-01'
+        and cd.weapon_cd not in ('vaporizer', 'hook', 'tuba')
+        group by 1, 2, 3
+        order by 1, 2
+        ;
+        """.format(year, year+1)
+
+    df = pd.read_sql(sql, conn)
+
+    # set up the plot
+    f, ax1 = plt.subplots(1, figsize=(20, 5))
+
+    # set the bar width
+    bar_width = 0.60
+
+    # the number of months present in the dataset
+    months = df["month"].unique()
+
+    # positions of the left bar-boundaries
+    bar_l = [i + 1 for i in range(len(months))]
+
+    # positions of the x-axis ticks (center of the bars as bar labels)
+    tick_pos = [i + (bar_width / 2) for i in bar_l]
+
+    # grid lines
+    ax1.set_axisbelow(True)
+    ax1.grid(b=True, which='major', color='#aaaaaa')
+
+    # tracking where the bottoms of the next bars to be drawn should go
+    bottoms = [0] * len(months)
+
+    # the height of each of the segments
+    segment_heights = [0] * len(months)
+
+    for i, weapon_cd in enumerate(df["weapon_cd"].unique()):
+
+        bar_color = weapon_colors[weapon_cd]
+        print("weapon code is {}, color is {}".format(weapon_cd, bar_color))
+
+        segment_df = df[df.weapon_cd == weapon_cd]
+
+        # create a bar plot, in position bar_1
+        ax1.bar(left=bar_l,
+                height=segment_df['actual'],
+                width=bar_width,
+                label=weapon_cd,
+                alpha=0.5,
+                color=bar_color,
+                bottom=bottoms
+                )
+
+        # update the bottoms, set the labels
+        for j, month in enumerate(months):
+            height = int(segment_df[df.month == month]['actual'])
+            segment_heights[j] = height
+
+            # put a label if the bar is big enough
+            if height/1000 > 10000:
+                ax1.text(j + 1.3, bottoms[j] + height/2, str(height/1000), size=8, ha='center')
+
+            # update the bottoms
+            bottoms[j] += height
+
+    # the tick marks along the bottom
+    plt.xticks(tick_pos, df['month'])
+
+    # the label along the Y axis
+    ax1.set_ylabel("Damage Points")
+
+    # the legend
+    plt.legend(loc='best', ncol=3, fontsize='small')
+
+    # set a buffer around the left and right edge
+    plt.xlim([min(tick_pos) - bar_width, max(tick_pos) + bar_width])
+
+    # a buffer above the top edge
+    plt.ylim([0, max(bottoms)+5500000])
+
+    # the label at the top of each bar w/ the total
+    for i, month in enumerate(months):
+        ax1.text(i + 1.3, bottoms[i] + 500000, str(bottoms[i]/1000), size=8, ha='center')
+
+    plt.title("Weapon Damage Per Month in {} (in thousands)".format(year))
+    plt.show()
+    # plt.savefig("{}_weapon_damage_per_month.png".format(year))
+
+
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("--year", type=int, help="Year to calculate")
@@ -240,9 +367,10 @@ def main():
     pg_pass = os.environ.get("PGPASS")
     conn = pg.connect(database="xonstatdb", user=pg_user, password=pg_pass, host="localhost")
 
-    games_per_month(conn, year)
-    players_per_month(conn, year)
-    hours_played(conn, year)
+    # games_per_month(conn, year)
+    # players_per_month(conn, year)
+    # hours_played(conn, year)
+    weapon_damage_per_month(conn, year)
 
 
 if __name__ == "__main__":
